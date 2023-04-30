333f6a7035865684719e16f1a87da73a7c6ccf06
BJTowne <bjtowne9@gmail.com>
2023-04-30 18:29:04 -0400

    Fix

--- settings/settings.md
+++ settings/settings.md
@@ -1,10 +1,11 @@
 # Settings
 
-Following are descriptions for all the settings that can be configured for the Code and Coffee Service.  
+Following are descriptions for all the settings that can be configured for the Code and Coffee Service.
 
 ## [Chapters](./settings/chapters.json)
-**chapters.json**  
-  
-A list of all the active Code and Coffee chapters. The name is a casual name to use as reference for the chapters for organizational purposes. The display name for a chapter should be taken from Meetup.com. meetupGroupUrlName is the chapter's Meetup group's url name. You can find it by navigating to the group's page in the url bar. This is used to fetch information for the group from Meetup.  
-  
+
+**chapters.json**
+
+A list of all the active Code and Coffee chapters. The name is a casual name to use as reference for the chapters for organizational purposes. The display name for a chapter should be taken from Meetup.com. meetupGroupUrlName is the chapter's Meetup group's url name. You can find it by navigating to the group's page in the url bar. This is used to fetch information for the group from Meetup.
+
 ![Group Url Name](./readme/group-url-name.png)
--- validate.ts
+++ validate.ts
@@ -1,24 +1,31 @@
-import {existsSync, readdirSync, readFileSync} from "fs";
-import {fileURLToPath} from "url";
-import {join} from "path";
+import { existsSync, readdirSync, readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { join } from "path";
 import Ajv from "ajv/dist/jtd.js";
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
-const ajv = new Ajv({allErrors: true});
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const ajv = new Ajv({ allErrors: true });
 
-for(const settingsFile of readdirSync(join(__dirname,'/settings'))){
-
-  if(!existsSync(join(__dirname,'/schemas',settingsFile))){
-    throw `Schema missing for "${settingsFile}"`
+for (const settingsFile of readdirSync(join(__dirname, "/settings")).filter(
+  (filename) => {
+    return filename.endsWith(".json");
   }
-  const schema = JSON.parse(readFileSync(join(__dirname,'/schemas',settingsFile),'utf-8'));
-  const settings = JSON.parse(readFileSync(join(__dirname,'/settings',settingsFile),'utf-8'));
+)) {
+  if (!existsSync(join(__dirname, "/schemas", settingsFile))) {
+    throw `Schema missing for "${settingsFile}"`;
+  }
+  const schema = JSON.parse(
+    readFileSync(join(__dirname, "/schemas", settingsFile), "utf-8")
+  );
+  const settings = JSON.parse(
+    readFileSync(join(__dirname, "/settings", settingsFile), "utf-8")
+  );
   const validate = ajv.compile(schema);
-  const valid = validate(settings)
-  if(!valid){
-    console.error(validate.errors)
-    throw `Invalid settings file "${settingsFile}"`
+  const valid = validate(settings);
+  if (!valid) {
+    console.error(validate.errors);
+    throw `Invalid settings file "${settingsFile}"`;
   }
 }
 
-console.log('All settings files are valid')
+console.log("All settings files are valid");
