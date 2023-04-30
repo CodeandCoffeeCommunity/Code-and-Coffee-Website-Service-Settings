53ab618e2a352ec0ecbf71aaf187e236244f57ac
BJTowne <bjtowne9@gmail.com>
2023-04-30 09:53:44 -0400

    Created validation script

--- validation/validate.ts
+++ validation/validate.ts
@@ -1,1 +1,24 @@
-console.log("this is a stand in file for the real validate.ts");
+import {existsSync, readdirSync, readFileSync} from "fs";
+import {fileURLToPath} from "url";
+import {join} from "path";
+import Ajv from "ajv/dist/jtd.js";
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const ajv = new Ajv({allErrors: true});
+
+for(const settingsFile of readdirSync(join(__dirname,'../settings'))){
+
+  if(!existsSync(join(__dirname,'/settings',settingsFile))){
+    throw `Validation file missing for "${settingsFile}"`
+  }
+  const schema = JSON.parse(readFileSync(join(__dirname,'/settings',settingsFile),'utf-8'));
+  const settings = JSON.parse(readFileSync(join(__dirname,'../settings',settingsFile),'utf-8'));
+  const validate = ajv.compile(schema);
+  const valid = validate(settings)
+  if(!valid){
+    console.error(validate.errors)
+    throw `Invalid settings file "${settingsFile}"`
+  }
+}
+
+console.log('All settings files are valid')
